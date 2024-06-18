b363c4e09bd958453a2d801fabc9e9e3a60d338b
Govind ghosh <govindghosh0@gmail.com>
2024-06-18 19:24:45 +0530

    [BUILD]featurecourses

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,9 +1,21 @@
+"use client";
 import Link from "next/link";
 import React from "react";
-import courseData from "../data/music_courses.json"
-
+import courseData from "../data/music_courses.json";
+import { BackgroundGradient } from "./ui/background-gradient";
+interface Course {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+}
 export default function FeaturedCourses() {
-          
+  const featuredCourse = courseData.courses.filter(
+    (course: Course) => course.isFeatured
+  );
   return (
     <div className="py-12 bg-gray-950">
       <div className="text-center">
@@ -14,11 +26,25 @@ export default function FeaturedCourses() {
           Learn With the Best
         </p>
       </div>
-      <div className="mt-10">
+      <div className="mt-10 m-6">
         <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-            test
+          {featuredCourse.map((course: Course) => (
+            <div key={course.id} className="flex justify-center">
+              <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                <div className="p-4 sm:p-6 flex flex-col items-center text-center text-black">
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                    {course.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {course.description}
+                  </p>
+                  <Link href={`/courses/${course.slug}`}>learn More.</Link>
+                </div>
+              </BackgroundGradient>
+            </div>
+          ))}
         </div>
-        </div>
+      </div>
       <div className="mt-20 text-center">
         <Link href={"/courses"} className="px-4 py-2 ">
           <button className="p-[3px] relative">
